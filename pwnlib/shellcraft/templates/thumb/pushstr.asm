849f9787557c6ed49d07873de84ccee181c08a54
Zach Riggle <riggle@google.com>
2015-10-12 02:04:50 -0400

    Fix doctest ellipsis

--- pwnlib/shellcraft/templates/thumb/pushstr.asm
+++ pwnlib/shellcraft/templates/thumb/pushstr.asm
@@ -19,7 +19,8 @@ on your version of binutils.
     >>> enhex(asm(shellcraft.pushstr('Hello\nWorld!', True))) in [
     ... '87ea070780b4dff8047001e0726c642180b4dff8047001e06f0a576f80b4dff8047001e048656c6c80b4',
     ... '87ea070780b4dff8067000f002b8726c642180b4dff8047000f002b86f0a576f80b4014f00f002b848656c6c80b4']
-    >>> print shellcraft.pushstr('abc')
+    True
+    >>> print shellcraft.pushstr('abc') #doctest: +ELLIPSIS
         /* push 'abc\x00' */
         ldr r7, value_...
         b value_..._after
